9d286b0c8823c5e893c8e3d183254144b773cb0c
beyond <921790476@qq.com>
2020-05-15 11:27:23 +0800

    add

--- src/main/java/com/wenhao/mybatisstudydemo/test/MybatisTest.java
+++ src/main/java/com/wenhao/mybatisstudydemo/test/MybatisTest.java
@@ -20,19 +20,23 @@ public class MybatisTest {
         SqlSession sqlSession1 = build.openSession();
         UserMapper mapper = sqlSession1.getMapper(UserMapper.class);
         //List<User> allUser = mapper.findAllUser();
-        User byUserId = mapper.findByUserId(1L);
+        //User byUserId = mapper.findByUserId(1L);
+
+        //sqlSession1.close();
+        User byUserId = sqlSession1.selectOne("com.wenhao.mybatisstudydemo.mapper.UserMapper.findByUserId", 1L);
+        System.out.println("第一次查询" + byUserId);
         sqlSession1.close();
-       /* User user = new User();
+        SqlSession sqlSession2 = build.openSession();
+        //UserMapper mapper1 = sqlSession2.getMapper(UserMapper.class);
+        //User byUserId1 = mapper1.findByUserId(1L);
+        User byUserId1 = sqlSession2.selectOne("com.wenhao.mybatisstudydemo.mapper.UserMapper.findByUserId", 1L);
+        System.out.println("第二次查询" + byUserId1);
+
+
+        /* User user = new User();
         user.setId(26L);
         user.setUsername("test");
         user.setPassword("test");
         mapper.updateUser(user);*/
-        SqlSession sqlSession2 = build.openSession();
-        UserMapper mapper1 = sqlSession2.getMapper(UserMapper.class);
-        User byUserId1 = mapper1.findByUserId(1L);
-        //System.out.println(allUser);
-        System.out.println("======");
-        System.out.println(byUserId);
-        System.out.println(byUserId1);
     }
 }
